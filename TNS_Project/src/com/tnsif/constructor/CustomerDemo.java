8b6f0111425144e791b4936520401ef31f14a465
musavveer <iammusavveer@gmail.com>
2023-03-26 15:51:19 +0530

    constructor

--- TNS_Project/src/com/tnsif/constructor/CustomerDemo.java
+++ TNS_Project/src/com/tnsif/constructor/CustomerDemo.java
@@ -6,28 +6,29 @@ public class CustomerDemo {
 
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);
-		
-		String name;
-		String city;
-		int id;
-		
-		System.out.println("Enter customer name");
-		name = sc.nextLine();
-		
-		System.out.println("Enter customer city");
-		city = sc.nextLine();
-		
-		System.out.println("Enter customer id");
-		id = sc.nextInt();
+//		Scanner sc = new Scanner(System.in);
+//		
+//		String name;
+//		String city;
+//		int id;
+//		
+//		System.out.println("Enter customer name");
+//		name = sc.nextLine();
+//		
+//		System.out.println("Enter customer city");
+//		city = sc.nextLine();
+//		
+//		System.out.println("Enter customer id");
+//		id = sc.nextInt();
 
 		
 		
-		Customer ob = new Customer();
-		ob.setCustomerName(name);
-		ob.setCustomerCity(city);
-		ob.setCustomerId(id);
-		System.out.println(name + " " + id);
+		Customer ob = new Customer("Musavveer", 15, "Bangalore");
+//		ob.setCustomerName("Musavveer");
+//		ob.setCustomerCity("Bangalore");
+//		ob.setCustomerId(15);
+//		
+//		System.out.println(ob.getCustomerName()+" "+ob.getCustomerCity()+" "+ob.getCustomerId());
 	}
 
 }
